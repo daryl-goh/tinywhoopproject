640a4b7203ddd29abea0767ff03554e73af5d497
daryl-goh <goh.daryl@gmail.com>
2022-12-02 10:01:07 +0800

    Initial Commit

---
+++ src/main/java/sg/edu/nus/iss/tinywhoopproject/TinywhoopprojectApplication.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.tinywhoopproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TinywhoopprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TinywhoopprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sg/edu/nus/iss/tinywhoopproject/TinywhoopprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.tinywhoopproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TinywhoopprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
