1dc296a1861a230350701d6a4fb73f242ab0fb30
daryl-goh <goh.daryl@gmail.com>
2022-12-04 16:03:59 +0800

    Added Controllers to link p ages

--- src/main/java/sg/edu/nus/iss/tinywhoopproject/controller/PilotController.java
+++ src/main/java/sg/edu/nus/iss/tinywhoopproject/controller/PilotController.java
@@ -1,8 +1,22 @@
 package sg.edu.nus.iss.tinywhoopproject.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class PilotController {
+
+    // Landing Page to Pilot List Page
+    @GetMapping(path={"/pilotlist"}) 
+    public String pilotlist(Model model) {
+        return "pilotlist";
+    }
+    
+    // Race One Pilots to Lap Timer Page
+    @GetMapping(path={"/laptimer"})
+    public String lapTimer(Model model) {
+        return "laptimer";
+    }
     
 }
--- src/main/java/sg/edu/nus/iss/tinywhoopproject/controller/RaceCourseController.java
+++ src/main/java/sg/edu/nus/iss/tinywhoopproject/controller/RaceCourseController.java
@@ -1,8 +1,21 @@
 package sg.edu.nus.iss.tinywhoopproject.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class RaceCourseController {
     
+    // Landing Page to Race Courses Page
+    @GetMapping(path={"/racecourses"})
+    public String raceCourses(Model model) {
+        return "racecourses";
+    }
+
+    // Race Courses to Race One Pilots Page
+    @GetMapping(path={"/raceonepilots"})
+    public String raceOnePilots(Model model) {
+        return "raceonepilots";
+    }
 }
