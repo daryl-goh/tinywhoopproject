25a6c3a5e0cfaa4e59e8477c6fafe10409aebe09
Yizhun Sim <simyizhun@Yizhuns-MacBook-Air.local>
2022-12-04 19:10:53 +0800

    TO do race course controller

--- src/main/java/sg/edu/nus/iss/tinywhoopproject/controller/RaceCourseController.java
+++ src/main/java/sg/edu/nus/iss/tinywhoopproject/controller/RaceCourseController.java
@@ -1,15 +1,25 @@
 package sg.edu.nus.iss.tinywhoopproject.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import sg.edu.nus.iss.tinywhoopproject.model.RaceCourse;
+import sg.edu.nus.iss.tinywhoopproject.services.RaceCourseService;
+
 @Controller
 public class RaceCourseController {
+    @Autowired
+    private RaceCourseService raceCourseService;
     
     // Landing Page to Race Courses Page
     @GetMapping(path={"/racecourses"})
     public String raceCourses(Model model) {
+        // List<RaceCourse> raceCourses = raceCourseService.retrieveAllRaceCourse();
+        // model.addAttribute("listOfRaceCourses", raceCourses);
         return "racecourses";
     }
 
